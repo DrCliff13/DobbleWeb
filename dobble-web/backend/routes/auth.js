4f1c6ff772dae1ce65b9fad521bea0df89f553ba
DrCliff13 <mdominguezs2@est.ups.edu.ec>
2025-06-13 12:18:38 -0500

    Update auth.js

--- dobble-web/backend/routes/auth.js
+++ dobble-web/backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const db = require('../db'); // o './db' si está en el mismo directorio
+
 
 router.post('/login', login);
 
