cbbbfe2ee5b91acc219a0a4d15b4e99a1a4be944
DrCliff13 <mdominguezs2@est.ups.edu.ec>
2025-06-13 12:48:20 -0500

    Update authRoutes.js

--- dobble-web/backend/routes/authRoutes.js
+++ dobble-web/backend/routes/authRoutes.js
@@ -4,10 +4,13 @@ const authController = require('../controllers/authController');
 const db = require('../db'); // o './db' si está en el mismo directorio
 
 
+
 // Rutas
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.get('/estadisticas/:id', authController.obtenerEstadisticas);
+// Agrega esta línea temporal
+router.get('/verificar-tabla', authController.verificarTabla);
 
 
 module.exports = router;
